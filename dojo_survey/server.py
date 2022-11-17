eecca6f7902433cd3ee0c806f04e5d0e880e10bf
Lauren Morgan <95324483+laurenmmorgs@users.noreply.github.com>
2022-11-16 16:19:03 -0800

    Add files via upload

---
+++ dojo_survey/server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'Unique key'
+
+
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def  processing():
+   session['name']= request.form['name']
+   session['location']= request.form['location']
+   session['language']= request.form['language']
+   session['comment'] = request.form['comment']
+   return redirect('/result')
+
+
+@app.route('/result')
+def  result():
+   return render_template('result.html')
+
+@app.route('/reset')
+def  reset():
+   session.clear()
+   return redirect('/')
+
+
+
+
+
+if __name__ == "__main__":
+   app.run(debug = True)
